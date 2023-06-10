ecb54251204d804c10b1b24027f3fe0ea057c95a
Natela <n.kerd@mail.ru>
2023-06-10 12:47:13 +0300

    created setTimeout function

--- homeWork_06.js
+++ homeWork_06.js
@@ -96,3 +96,30 @@ Promise.resolve().then(() => console.log("3"));
 console.log("4");
 
 // Ответ: 1, 4, 3, 2
+
+// 6) Напишите функцию, которая будет проходить через массив целых чисел и выводить индекс каждого элемента с задержкой в 3 секунды.
+// Входные данные: [10, 12, 15, 21]
+
+const arr = [10, 12, 15, 21]
+
+// задержка 3сек для всех индексов разом
+function foo(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        setTimeout(() => { console.log(i) }, 3000)
+    }
+}
+
+foo(arr);
+
+// задержка 3сек перед выводом каждого индекса
+function foo(arr) {
+    const startTimeout = 3000;
+
+    for (let i = 0; i < arr.length; i++) {
+        setTimeout(() => { console.log(i) }, startTimeout*(i+1))
+    }
+}
+
+foo(arr);
+
+
